488aa3cdadc3f05d379a4b7025567120c26668b5
CollinHU <humy720@163.com>
2023-11-02 19:32:08 +0800

    update utils

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,7 @@
 class Utils:
     @staticmethod
     def get_openai_key():
-        return "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"
+        return ""
     @staticmethod
     def get_tongyi_key():
         return ""
