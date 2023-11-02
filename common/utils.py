83b2b840e5607c07b9e4c2ddedac711017be217c
CollinHU <humy720@163.com>
2023-11-02 19:14:43 +0800

    update agent

--- common/utils.py
+++ common/utils.py
@@ -1,11 +1,7 @@
 class Utils:
     @staticmethod
     def get_openai_key():
-<<<<<<< HEAD
-        return ""
+        return "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"
     @staticmethod
     def get_tongyi_key():
         return ""
-=======
-        return "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"
->>>>>>> main
